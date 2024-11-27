3bf9f8d99e5dafd35961273b83c157754ba5a575
sharmi2209 <sharmiladevigopi2002@gmail.com>
2024-11-27 12:05:00 +0530

    Add files via upload

--- CASE STUDY-3.sql
+++ CASE STUDY-3.sql
@@ -14,4 +14,43 @@ SELECT * FROM student;
 
 CREATE TABLE courses (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50) NOT NULL);
 
-INSERT INTO
+INSERT INTO  courses (name) VALUES
+('IT'),
+('EEE'),
+('ECE'),
+('MECH');
+
+SELECT * FROM  courses;
+
+CREATE TABLE Enrollments (student_id INT,course_id INT,grade CHAR(1) ,PRIMARY KEY (student_id, course_id),
+FOREIGN KEY (student_id) REFERENCES Student(id),FOREIGN KEY (course_id) REFERENCES Courses(id));
+
+INSERT INTO Enrollments (student_id,course_id,grade) VALUES
+(1, 3, 'A'),
+(2, 2, 'B'),
+(3, 4, 'D'),
+(4, 1, 'B'),
+(5, 2, 'A');
+
+SELECT * FROM  Enrollments;
+
+-- List all students --
+
+SELECT * FROM student;
+
+-- List all courses --
+
+SELECT * FROM  courses;
+
+ -- Show the names of students and the courses they are enrolled in --
+
+SELECT student.name AS stu_name ,courses.name AS course_name FROM student JOIN Enrollments ON student.id= Enrollments.student_id
+JOIN Courses  ON Enrollments.Course_id = Courses.id;
+
+-- Count the number of students in each course --
+
+SELECT  Courses.name as courses_name,COUNT(Enrollments.student_id) FROM Courses LEFT JOIN 
+Enrollments ON Courses.id = Enrollments.course_id GROUP BY Courses.id;
+
+
+
